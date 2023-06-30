163265a05b0ad24de0498a89ec1b084eef806f60
Pavel Burgr <pb@drake.cz>
2023-06-30 20:08:30 +0200

    WIP

---
+++ fw-zig/zig-cache/o/a03a214c3da4471b5a5063c68cddb66c/builtin.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+/// Zig version. When writing code that supports multiple versions of Zig, prefer
+/// feature detection (i.e. with `@hasDecl` or `@hasField`) over version checks.
+pub const zig_version = std.SemanticVersion.parse(zig_version_string) catch unreachable;
+pub const zig_version_string = "0.11.0-dev.2227+f9b582950";
+pub const zig_backend = std.builtin.CompilerBackend.stage2_llvm;
+
+pub const output_mode = std.builtin.OutputMode.Exe;
+pub const link_mode = std.builtin.LinkMode.Static;
+pub const is_test = false;
+pub const single_threaded = true;
+pub const abi = std.Target.Abi.eabi;
+pub const cpu: std.Target.Cpu = .{
+    .arch = .riscv32,
+    .model = &std.Target.riscv.cpu.generic_rv32,
+    .features = std.Target.riscv.featureSet(&[_]std.Target.riscv.Feature{
+        .c,
+        .m,
+    }),
+};
+pub const os = std.Target.Os{
+    .tag = .freestanding,
+    .version_range = .{ .none = {} },
+};
+pub const target = std.Target{
+    .cpu = cpu,
+    .os = os,
+    .abi = abi,
+    .ofmt = object_format,
+};
+pub const object_format = std.Target.ObjectFormat.elf;
+pub const mode = std.builtin.Mode.Debug;
+pub const link_libc = false;
+pub const link_libcpp = false;
+pub const have_error_return_tracing = true;
+pub const valgrind_support = false;
+pub const sanitize_thread = false;
+pub const position_independent_code = false;
+pub const position_independent_executable = false;
+pub const strip_debug_info = false;
+pub const code_model = std.builtin.CodeModel.default;
